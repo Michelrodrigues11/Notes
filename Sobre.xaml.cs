da7398ddd366d3e25b98de5771da8947cce1e430
Michel <henriquemichel10@gmail.com>
2023-07-28 22:46:01 -0300

    first commit

---
+++ Sobre.xaml.cs
@@ -0,0 +1,15 @@
+namespace Notes;
+
+public partial class Sobre : ContentPage
+{
+	public Sobre()
+	{
+		InitializeComponent();
+	}
+
+    private async void LearnMore_Clicked(object sender, EventArgs e)
+    {
+        // Navigate to the specified URL in the system browser.
+        await Launcher.Default.OpenAsync("https://aka.ms/maui");
+    }
+}
